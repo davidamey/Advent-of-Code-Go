f8daebe730cb850a32f46cbfd6706bfd900a7b6d
David Amey <david@amey.org.uk>
2018-12-12 15:32:55 +0000

    Improved 2018/11 efficiency

--- 2018/11/11.go
+++ 2018/11/11.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	"advent/util"
 	"fmt"
 	"time"
 )
 
 func main() {
 	// example()
-	part1()
-	part2()
+	solve(18, 300)
+	solve(42, 300)
+	solve(3463, 300)
+	// part1()
+	// part2()
 }
 
 func example() {
-	fmt.Println("3,5 @ 8: ", power(3, 5, 8))           // 4
-	fmt.Println("122,79 @ 57: ", power(122, 79, 57))   // -5
-	fmt.Println("217,196 @ 39: ", power(217, 196, 39)) // 0
-	fmt.Println("101,153 @ 71: ", power(101, 153, 71)) // 4
+	fmt.Println("3,5 @ 8: ", powerFuncForSerial(8)(3, 5))           // 4
+	fmt.Println("122,79 @ 57: ", powerFuncForSerial(57)(122, 79))   // -5
+	fmt.Println("217,196 @ 39: ", powerFuncForSerial(39)(217, 196)) // 0
+	fmt.Println("101,153 @ 71: ", powerFuncForSerial(71)(101, 153)) // 4
 
-	// g1 := makeGrid(18)
+	// g1 := NewGrid(powerFuncForSerial(18), 300, 300)
 	// g1.Print(32, 44, 36, 50)
 	// fmt.Println("33,45", g1["33,45"])
 	// fmt.Println("33,46", g1["33,46"])
@@ -33,116 +35,170 @@ func example() {
 	// fmt.Printf("serial 42 gives maxPower of %d at %d,%d\n", maxPowerForGridP1(makeGrid(42)))
 }
 
-func part1() {
-	fmt.Println("== part1 == ")
-	maxPower, x, y := maxPowerForGridP1(makeGrid(3463))
-	fmt.Printf("serial 3463 gives maxPower of %d at %d,%d\n", maxPower, x, y)
-}
-
-func part2() {
+func solve(serial, gridSize int) {
 	start := time.Now()
-	var powerMax, xMax, yMax, sizeMax int
+	defer func() { fmt.Printf(" in %fs\n", time.Since(start).Seconds()) }()
 
-	g := makeGrid(3463)
-	summer := NewSummer(&g)
+	g := NewGrid(powerFuncForSerial(serial), gridSize, gridSize)
 
-	for size := 1; size < 300; size++ {
-		if size%10 == 0 {
-			fmt.Printf("* checking size %d\n", size)
+	maxSum := 0
+	maxX := 0
+	maxY := 0
+	maxSize := 0
+	sums := NewGrid(func(x, y int) int {
+		if x+2 >= gridSize || y+2 >= gridSize {
+			return 0
 		}
-		for x := 1; x <= 300-size+1; x++ {
-			for y := 1; y <= 300-size+1; y++ {
-				power := summer.Sum(size, x, y)
-				if power > powerMax {
-					powerMax = power
-					sizeMax = size
-					xMax = x
-					yMax = y
-				}
+
+		sum := 0
+		for i := 0; i < 3; i++ {
+			for j := 0; j < 3; j++ {
+				sum += g[y+j][x+i]
 			}
 		}
-	}
 
-	fmt.Println("== part2 ==")
-	fmt.Printf("max power of %d achieved at %d,%d with square size %d\n", powerMax, xMax, yMax, sizeMax)
-	fmt.Printf("took %fs", time.Since(start).Seconds())
-}
+		if sum > maxSum {
+			maxSum = sum
+			maxX = x
+			maxY = y
+			maxSize = 3
+		}
 
-type Summer struct {
-	grid *util.Grid
-	sums map[string]int
-}
-
-func NewSummer(grid *util.Grid) *Summer {
-	return &Summer{
-		grid,
-		make(map[string]int),
-	}
-}
-
-func (s *Summer) Loc(size, x, y int) string {
-	return fmt.Sprintf("%d,%d,%d", size, x, y)
-}
-
-func (s *Summer) Sum(size, x, y int) int {
-	loc := s.Loc(size, x, y)
-
-	if size == 1 {
-		return s.grid.Get(x, y)
-	}
-
-	if sum, ok := s.sums[loc]; ok {
 		return sum
-	}
+	}, gridSize, gridSize)
 
-	sum := s.Sum(size-1, x, y)
-	for i := 0; i < size; i++ {
-		sum += s.grid.Get(x+i, y+size-1)
-	}
-	for j := 0; j < size-1; j++ {
-		sum += s.grid.Get(x+size-1, y+j)
-	}
-	s.sums[loc] = sum
-	return sum
-}
+	// fmt.Println("maxSum|maxX|maxY|maxSize", maxSum, maxX+1, maxY+1, maxSize)
 
-func maxPowerForGridP1(g util.Grid) (maxPower, xMax, yMax int) {
-	for x := 1; x <= 300-2; x++ {
-		for y := 1; y <= 300-2; y++ {
-			power := 0
-			for i := 0; i < 3; i++ {
-				for j := 0; j < 3; j++ {
-					loc := fmt.Sprintf("%d,%d", x+i, y+j)
-					power += g[loc]
+	for size := 4; size < 300; size++ {
+		for y := 0; y+size < gridSize; y++ {
+			for x := 0; x+size < gridSize; x++ {
+				for i := 0; i < size; i++ {
+					sums[y][x] += g[y+size-1][x+i] + g[y+i][x+size-1]
+				}
+				sums[y][x] += g[y+size-1][x+size-1]
+				if sums[y][x] > maxSum {
+					maxSum = sums[y][x]
+					maxX = x
+					maxY = y
+					maxSize = size
 				}
 			}
-			if power > maxPower {
-				maxPower = power
-				xMax = x
-				yMax = y
-			}
 		}
 	}
-	return
+
+	fmt.Printf("%d: maxSum of %d found at %d,%d,%d", serial, maxSum, maxX+1, maxY+1, maxSize)
 }
 
-func makeGrid(serial int) util.Grid {
-	grid := make(util.Grid)
-	for x := 1; x <= 300; x++ {
-		for y := 1; y <= 300; y++ {
-			grid.Set(x, y, power(x, y, serial))
+func powerFuncForSerial(serial int) func(x, y int) int {
+	return func(x, y int) int {
+		// problem uses 1-based
+		x += 1
+		y += 1
+
+		rackID := x + 10
+		power := rackID * y
+		power += serial
+		power *= rackID
+		power = power / 100 % 10
+		power -= 5
+
+		return power
+	}
+}
+
+type Grid [][]int
+
+func NewGrid(f func(x, y int) int, dx, dy int) Grid {
+	g := make(Grid, dy)
+	for y := 0; y < dy; y++ {
+		g[y] = make([]int, dx)
+		for x := 0; x < dx; x++ {
+			g[y][x] = f(x, y)
 		}
 	}
-	return grid
+	return g
 }
 
-func power(x, y, serial int) int {
-	rackID := x + 10
-	power := rackID * y
-	power += serial
-	power *= rackID
-	power = power / 100 % 10
-	power -= 5
-
-	return power
+func (g Grid) Print(x1, y1, x2, y2 int) {
+	for y := y1; y <= y2; y++ {
+		for x := x1; x <= x2; x++ {
+			fmt.Printf(" %2d ", g[y][x])
+		}
+		fmt.Println()
+	}
+	fmt.Println()
 }
+
+/** original inefficient code below **/
+
+// func part1() {
+// 	fmt.Println("== part1 == ")
+// 	maxPower, x, y := maxPowerForGridP1(makeGrid(3463))
+// 	fmt.Printf("serial 3463 gives maxPower of %d at %d,%d\n", maxPower, x, y)
+// }
+
+// func part2() {
+// 	start := time.Now()
+// 	var powerMax, xMax, yMax, sizeMax int
+
+// 	g := makeGrid(3463)
+// 	summer := NewSummer(&g)
+
+// 	for size := 1; size < 300; size++ {
+// 		if size%10 == 0 {
+// 			fmt.Printf("* checking size %d\n", size)
+// 		}
+// 		for x := 1; x <= 300-size+1; x++ {
+// 			for y := 1; y <= 300-size+1; y++ {
+// 				power := summer.Sum(size, x, y)
+// 				if power > powerMax {
+// 					powerMax = power
+// 					sizeMax = size
+// 					xMax = x
+// 					yMax = y
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	fmt.Println("== part2 ==")
+// 	fmt.Printf("max power of %d achieved at %d,%d with square size %d\n", powerMax, xMax, yMax, sizeMax)
+// 	fmt.Printf("took %fs", time.Since(start).Seconds())
+// }
+
+// type Summer struct {
+// 	grid *util.Grid
+// 	sums map[string]int
+// }
+
+// func NewSummer(grid *util.Grid) *Summer {
+// 	return &Summer{
+// 		grid,
+// 		make(map[string]int),
+// 	}
+// }
+
+// func (s *Summer) Loc(size, x, y int) string {
+// 	return fmt.Sprintf("%d,%d,%d", size, x, y)
+// }
+
+// func (s *Summer) Sum(size, x, y int) int {
+// 	loc := s.Loc(size, x, y)
+
+// 	if size == 1 {
+// 		return s.grid.Get(x, y)
+// 	}
+
+// 	if sum, ok := s.sums[loc]; ok {
+// 		return sum
+// 	}
+
+// 	sum := s.Sum(size-1, x, y)
+// 	for i := 0; i < size-1; i++ {
+// 		sum += s.grid.Get(x+i, y+size-1)
+// 		sum += s.grid.Get(x+size-1, y+i)
+// 	}
+// 	sum += s.grid.Get(x+size, y+size)
+// 	s.sums[loc] = sum
+// 	return sum
+// }
