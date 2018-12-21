95328e157581988e134fcc90736b1521cbbae3eb
David Amey <david@amey.org.uk>
2018-12-21 22:24:08 +0000

    add up/down/left/right methods to point

--- util/grid.go
+++ util/grid.go
@@ -6,6 +6,19 @@ type Point struct {
 	X, Y int
 }
 
+func (p *Point) Left() Point {
+	return Point{p.X - 1, p.Y}
+}
+func (p *Point) Right() Point {
+	return Point{p.X + 1, p.Y}
+}
+func (p *Point) Up() Point {
+	return Point{p.X, p.Y - 1}
+}
+func (p *Point) Down() Point {
+	return Point{p.X, p.Y + 1}
+}
+
 func (p *Point) Adjacent(withCorners bool) []Point {
 	if withCorners {
 		return []Point{
