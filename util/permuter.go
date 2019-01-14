92ed5994aa7a809ceb496b2d5b417b9e33bf92bf
David Amey <david@amey.org.uk>
2019-01-14 16:26:13 +0000

    Added permuter util

---
+++ util/permuter.go
@@ -0,0 +1,92 @@
+package util
+
+type IntPermuter struct {
+	slice []int
+	perm  []int
+}
+
+type StringPermuter struct {
+	slice []string
+	perm  []int
+}
+
+func NewIntPermuter(slice []int) *IntPermuter {
+	return &IntPermuter{
+		slice: slice,
+		perm:  make([]int, len(slice)),
+	}
+}
+
+func NewStringPermuter(slice []string) *StringPermuter {
+	return &StringPermuter{
+		slice: slice,
+		perm:  make([]int, len(slice)),
+	}
+}
+
+func (p *IntPermuter) NextPerm() []int {
+	if p.perm[0] >= len(p.perm) {
+		return nil
+	}
+
+	result := make([]int, len(p.slice))
+	copy(result, p.slice)
+	for i, v := range p.perm {
+		result[i], result[i+v] = result[i+v], result[i]
+	}
+
+	cyclePerm(p.perm)
+	return result
+}
+
+func (p *StringPermuter) NextPerm() []string {
+	if p.perm[0] >= len(p.perm) {
+		return nil
+	}
+
+	result := make([]string, len(p.slice))
+	copy(result, p.slice)
+	for i, v := range p.perm {
+		result[i], result[i+v] = result[i+v], result[i]
+	}
+
+	cyclePerm(p.perm)
+	return result
+}
+
+func (p *IntPermuter) Permutations() chan []int {
+	ch := make(chan []int)
+
+	go func() {
+		defer close(ch)
+		for next := p.NextPerm(); next != nil; next = p.NextPerm() {
+			ch <- next
+		}
+	}()
+
+	return ch
+}
+
+func (p *StringPermuter) Permutations() chan []string {
+	ch := make(chan []string)
+
+	go func() {
+		defer close(ch)
+		for next := p.NextPerm(); next != nil; next = p.NextPerm() {
+			ch <- next
+		}
+	}()
+
+	return ch
+}
+
+func cyclePerm(perm []int) {
+	// Cycle the permutation
+	for i := len(perm) - 1; i >= 0; i-- {
+		if i == 0 || perm[i] < len(perm)-i-1 {
+			perm[i]++
+			break
+		}
+		perm[i] = 0
+	}
+}
