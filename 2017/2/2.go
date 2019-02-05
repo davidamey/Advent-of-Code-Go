20691bd3837c6d2296b88a81a8a2a72d688cfd92
David Amey <david@amey.org.uk>
2019-02-05 20:56:21 +0000

    Added 2017/2

---
+++ 2017/2/2.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"advent/util"
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	// lines := util.MustReadFileToLines("example")
+	lines := util.MustReadFileToLines("input")
+
+	checksumP1 := 0
+	checksumP2 := 0
+	for _, l := range lines {
+		p1, p2 := minMax(l)
+		checksumP1 += p1
+		checksumP2 += p2
+	}
+
+	fmt.Println("p1=", checksumP1)
+	fmt.Println("p2=", checksumP2)
+}
+
+func minMax(s string) (p1, p2 int) {
+	min := 1 << 16
+	max := -1 << 16
+	strs := regexp.MustCompile(`\s+`).Split(s, -1)
+
+	nums := make([]int, len(strs))
+	for i, n := range strs {
+		nums[i], _ = strconv.Atoi(n)
+	}
+
+	for _, n := range nums {
+		if n < min {
+			min = n
+		}
+		if n > max {
+			max = n
+		}
+
+		if p2 == 0 {
+			for _, n2 := range nums {
+				if n != n2 && n%n2 == 0 {
+					p2 = n / n2
+					break
+				}
+			}
+		}
+	}
+	return max - min, p2
+}
