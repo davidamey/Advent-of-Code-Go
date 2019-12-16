1be89f675aa0c2b50119f24895ab590773227ed4
David Amey <david@amey.org.uk>
2019-12-16 18:03:28 +0000

    2019 day 13

---
+++ 2019/13/13.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"advent-of-code-go/2019/intcode"
+	"advent-of-code-go/util"
+	"advent-of-code-go/util/grid"
+	"fmt"
+	"sync"
+	"time"
+)
+
+const (
+	tEmpty = iota
+	tWall
+	tBlock
+	tPaddle
+	tBall
+)
+
+func main() {
+	defer util.Duration(time.Now())
+
+	prog := intcode.Program(util.MustReadCSInts("input"))
+
+	fmt.Println("p1=", p1(prog))
+	fmt.Println("p2=", p2(prog))
+}
+
+func p1(prog intcode.Program) (count int) {
+	for i, out := 0, prog.Run(); i < len(out); i += 3 {
+		if out[i+2] == tBlock {
+			count++
+		}
+	}
+	return
+}
+
+func p2(prog intcode.Program) (score int) {
+	g := game{
+		in:      make(chan int),
+		out:     make(chan int),
+		running: true,
+		grid:    grid.New(),
+	}
+
+	prog[0] = 2
+	go func() {
+		prog.RunBuf("arcade", g.in, g.out)
+		g.running = false
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		var e [3]int
+		read := 0
+		for g.running {
+			select {
+			case g.in <- g.getJoyDir():
+			case v := <-g.out:
+				e[read] = v
+				read++
+				if read == 3 {
+					read = 0
+					g.parseEntity(e)
+				}
+			}
+
+			// // print:
+			// g.grid.Print("%c", true)
+			// fmt.Println("score=", g.score)
+			// time.Sleep(10 * time.Millisecond)
+		}
+	}()
+
+	wg.Wait()
+	return g.score
+}
+
+type game struct {
+	in, out     chan int
+	ballX, padX int
+	score       int
+	running     bool
+	grid        *grid.Grid
+}
+
+func (g *game) getJoyDir() int {
+	switch {
+	case g.ballX > g.padX:
+		return 1
+	case g.ballX < g.padX:
+		return -1
+	default:
+		return 0
+	}
+}
+
+func (g *game) parseEntity(e [3]int) {
+	if e[0] == -1 && e[1] == 0 {
+		g.score = e[2]
+	} else {
+		switch e[2] {
+		case tBall:
+			g.ballX = e[0]
+		case tPaddle:
+			g.padX = e[0]
+		}
+		g.grid.SetAt(e[0], e[1], mapType(e[2]))
+	}
+}
+
+func mapType(t int) rune {
+	switch t {
+	case tWall:
+		return '█'
+	case tBlock:
+		return '▭'
+	case tPaddle:
+		return '_'
+	case tBall:
+		// return '◯'
+		return '☃' // snowman!
+	default:
+		return ' '
+	}
+}
