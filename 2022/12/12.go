5b8817074a5c19c26bec468e7f09adbdcd780ac9
David Amey <david@amey.org.uk>
2022-12-21 20:22:19 +0000

    Add 2022/12

---
+++ 2022/12/12.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"advent-of-code-go/util"
+	"advent-of-code-go/util/grid"
+	"advent-of-code-go/util/vector"
+	"fmt"
+	"math"
+	"time"
+)
+
+func main() {
+	defer util.Duration(time.Now())
+
+	// lines := util.MustReadFileToLines("example")
+	lines := util.MustReadFileToLines("input")
+
+	g := grid.FromLines(lines)
+
+	var starts []vector.Vec
+	var end vector.Vec
+	g.ForEach(func(v vector.Vec, r interface{}) {
+		switch r.(rune) {
+		case 'S':
+			starts = append([]vector.Vec{v}, starts...)
+			g.Set(v, 'a')
+		case 'a':
+			starts = append(starts, v)
+		case 'E':
+			end = v
+			g.Set(v, 'z')
+		}
+	})
+
+	pathLengths := make([]int, len(starts))
+	for i, s := range starts {
+		pathLengths[i] = solve(g, s, end)
+	}
+
+	fmt.Println("p1=", pathLengths[0])
+	fmt.Println("p2=", util.MinInt(pathLengths...))
+}
+
+func solve(g *grid.Grid, start, end vector.Vec) int {
+	n := g.ShortestPath(start, end, func(v, parent interface{}, depth int) bool {
+		return v.(rune) <= parent.(rune)+1
+	})
+	if n == nil {
+		return math.MaxInt
+	}
+	return n.Length
+}
